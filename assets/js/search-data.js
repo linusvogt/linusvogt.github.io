8344c311a8453925160f422914ea8205d17d3a63
linusvogt <34556737+linusvogt@users.noreply.github.com>
2025-02-17 15:19:26 +0000

    Deploying to gh-pages from @ linusvogt/linusvogt.github.io@1dac00db095acd644870c619ea1263a756bfccaa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "news-defended-my-phd-read-the-thesis-manuscript-here-todo",
-          title: 'Defended my PhD! Read the thesis manuscript here (TODO)',
+        },{id: "news-defended-my-phd-read-the-thesis-manuscript-here",
+          title: 'Defended my PhD! Read the thesis manuscript here',
           description: "",
           section: "News",},{id: "news-our-paper-detecting-no-amoc-decline-since-the-1960s-was-published-in-nature-communications",
           title: 'Our paper detecting no AMOC decline since the 1960s was published in Nature...',
